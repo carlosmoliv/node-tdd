3392aaf00e56be97071603d0df3ad0574d9fc167
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-11-25 18:20:36 -0300

    feat: ensure DeleteEvent calls LoadGroupRepo with correct eventId

---
+++ tests/domain/use-cases/delete-event.spec.ts
@@ -0,0 +1,34 @@
+class DeleteEvent {
+  constructor (
+    private readonly loadGroupRepo: LoadGroupRepo
+  ) {}
+
+  async perform ({ id }: { id: string, userId: string }): Promise<void> {
+    await this.loadGroupRepo.load({ eventId: id })
+  }
+}
+
+interface LoadGroupRepo {
+  load: (input: { eventId: string }) => Promise<void>
+}
+
+class LoadGroupRepoSpy implements LoadGroupRepo {
+  eventId: string | undefined
+
+  async load ({ eventId }: { eventId: string }): Promise<void> {
+    this.eventId = eventId
+  }
+}
+
+describe('DeleteEvent', () => {
+  it('should get group data', async () => {
+    const id = 'any_event_id'
+    const userId = 'any_user_id'
+    const loadGroupRepo = new LoadGroupRepoSpy()
+    const sut = new DeleteEvent(loadGroupRepo)
+
+    await sut.perform({ id, userId })
+
+    expect(loadGroupRepo.eventId).toBe(id)
+  })
+})
